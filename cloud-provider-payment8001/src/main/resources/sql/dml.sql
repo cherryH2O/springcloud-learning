f5caa120cd6863d552e36c8886fd8de527840415
cherryH2O <947579019@qq.com>
2021-02-18 18:28:26 +0800

    add init.

---
+++ cloud-provider-payment8001/src/main/resources/sql/dml.sql
@@ -0,0 +1,7 @@
+INSERT INTO `payment` VALUES (1,'atguigu001');
+INSERT INTO `payment` VALUES (2,'atguigu002');
+INSERT INTO `payment` VALUES (3,'atguigu003');
+INSERT INTO `payment` VALUES (4,'atguigu004');
+
+
+# 1.0===========================================
